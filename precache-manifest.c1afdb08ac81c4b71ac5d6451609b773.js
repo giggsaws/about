a146c59dacf871cf2f5247c316d5de6972ccdbdb
ihsandroid1 <maenbos30@gmail.com>
2020-05-20 02:02:12 +0700

    Updates

---
+++ precache-manifest.c1afdb08ac81c4b71ac5d6451609b773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f816fe31b354c4257775925a0ba6f7",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "92f5155c33eefa2a670d",
+    "url": "/about/static/css/main.9d73cdcc.chunk.css"
+  },
+  {
+    "revision": "c24afd011b05647dce96",
+    "url": "/about/static/js/2.9ab0b21c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about/static/js/2.9ab0b21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f5155c33eefa2a670d",
+    "url": "/about/static/js/main.e8696d52.chunk.js"
+  },
+  {
+    "revision": "ba95aae0fbaeb44dc6bb",
+    "url": "/about/static/js/runtime-main.acf8c6a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about/static/media/logo.5d5d9eef.svg"
+  }
+]);
